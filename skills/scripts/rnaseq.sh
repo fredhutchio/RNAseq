f91ac4f7e6a729f1e7397791f6b047335b106b63
Kate Hertweck <k8hertweck@gmail.com>
2021-03-11 08:39:43 -0800

    framing reproducibility

--- skills/scripts/rnaseq.sh
+++ skills/scripts/rnaseq.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 ## Shell script to pre-process data for bulk RNAseq
+
+## Setup and organization
+
+## Quality assessment and filtering of raw genomic data
+
+## Read mapping and summary
